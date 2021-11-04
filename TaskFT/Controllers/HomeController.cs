c480597d3f26398d9d1d4356c1ffd0b06ff45309
09092021 <09092021@DESKTOP-I8RNFBJ>
2021-11-04 16:24:24 +0400

    added get of imgs

--- TaskFT/Controllers/HomeController.cs
+++ TaskFT/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -14,15 +12,18 @@ using System.Web.Helpers;
 using System.Drawing;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
+using Microsoft.Extensions.FileProviders;
 
 namespace TaskFT.Controllers
 {
     public class HomeController : Controller
     {
         AppDbContext _db;
-        public HomeController(AppDbContext db)
+        private IHostingEnvironment Environment;
+        public HomeController(AppDbContext db, IHostingEnvironment environment)
         {
             _db = db;
+            Environment = environment;
         }
 
         public IActionResult Index()
@@ -33,27 +34,29 @@ namespace TaskFT.Controllers
         [HttpPost]
         public IActionResult Index(IFormFile postedFile)
         {
+           // string wwwPath = Environment.WebRootPath;
 
-            string path = ("wwwroot/Images");
-            if (!Directory.Exists(path))
+            string path = "Images";
+            string newPath = "wwwroot/Images";
+            if (!Directory.Exists(newPath))
             {
-                Directory.CreateDirectory(path);
+                Directory.CreateDirectory(newPath);
             }
 
             string fileName = Path.GetFileName(postedFile.FileName);
-            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            using (FileStream stream = new FileStream(Path.Combine(newPath, fileName), FileMode.Create))
             {
                 postedFile.CopyTo(stream);
                 ViewBag.Message += $"<b>{fileName}</b> uploaded.<br />";
 
                 var image = Image.Load(postedFile.OpenReadStream());
                 image.Mutate(x => x.Resize(256, 256));
-                image.Save(Path.Combine(path, "_256" + fileName));
+                image.Save(Path.Combine(newPath, "_256" + fileName));
                 image.Mutate(x => x.Resize(56, 56));
-                image.Save(Path.Combine(path, "_56" + fileName));
+                image.Save(Path.Combine(newPath, "_56" + fileName));
 
                 image.Mutate(x => x.Crop(30, 30));
-                image.Save(Path.Combine(path, "cropped" + fileName));
+                image.Save(Path.Combine(newPath, "cropped" + fileName));
 
                 List<ImgModel> imgs = new List<ImgModel>();
                 imgs.Add(new ImgModel { Name = fileName, Path = Path.Combine(path, fileName) });
@@ -70,7 +73,17 @@ namespace TaskFT.Controllers
 
         public IActionResult Privacy()
         {
+
             var imgs = _db.Images.ToList();
+
+            //foreach (var item in imgs)
+            //{
+            //    ImageResponseModel image = new ImageResponseModel
+            //    {
+            //        ImagePath = "/Images/" + item.Name
+            //    };
+            //    imageResponseModels.Add(image);
+            //}
             return View(imgs);
         }
 
@@ -79,6 +92,13 @@ namespace TaskFT.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    static class _Path
+    {
+        public static string Combine(string firstPart,string secondPart)
+        {
+            return $"{firstPart}/{secondPart}";
         }
     }
 }
